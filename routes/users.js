3be5b12f08a5eb2a34b7e9170d490f77f710c211
bobtheburgerflipper <t9jojo01@students.oamk.fi>
2022-02-24 15:42:17 +0200

    schema validation working

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,20 @@ const bcrypt = require("bcryptjs")
 const passport = require("passport")
 const basicStrategy = require("passport-http").BasicStrategy
 const users = require('../data/users')
+const Ajv = require('ajv')
+const ajv = new Ajv()
+const userSchema = require('../schemas/user.schema.json')
 
+const userInfoValidator = ajv.compile(userSchema)
+const userInfoValidateMw = function (req, res, next) {
+    const validationResult = userInfoValidator(req.body)
+    if(validationResult) {
+        next()
+    }
+    else {
+        res.sendStatus(400)
+    }
+}
 
 router.get('/', passport.authenticate("jwt", {session: false}), (req, res) => {
     res.json(users)
@@ -22,20 +35,20 @@ router.get('/:id', (req, res) => {
     } 
 })
 
-router.post('/', (req, res) => {
+router.post('/', userInfoValidateMw, (req, res) => { 
     const salt = bcrypt.genSaltSync(6)
     const hashedPassword = bcrypt.hashSync(req.body.password, salt)
-  
+      
     const user = {
-      id: uuidv4(),
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      phoneNumber: req.body.phoneNumber,
-      password: hashedPassword      
+        id: uuidv4(),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        password: hashedPassword      
     } 
     users.push(user)
-
+    
     res.sendStatus(201)
 })
 
@@ -58,7 +71,7 @@ const secrets = require('../secrets.json')
 
 router.post('/login', passport.authenticate("basic", {session: false}), (req, res) => {
     // generate and return JWT upon succesful login
-    // we use the token for future authentication
+    // use the token for future authentication
     const payloadData = {
         foo: "bar",
         hops: "pops",
